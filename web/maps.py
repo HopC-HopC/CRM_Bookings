0fdcb2fba3404bf0b44d0198e66636fe611d037e
HopC-HopC <79705279+HopC-HopC@users.noreply.github.com>
2021-10-19 11:11:23 +0100

    Update maps.py

--- web/maps.py
+++ web/maps.py
@@ -1,9 +1,6 @@
 # from flask_googlemaps import get_address, get_coordinates
 from googlemaps import Client as GoogleMaps
 
-API_KEY = "AIzaSyDbYrFwu-5Nftk1f2xc_lBZkMFSR98YGps"
+API_KEY = ""
 
 gmaps = GoogleMaps(key=API_KEY)
-
-#Geocoding: getting coordinates from address text
-print(gmaps.geocode('57 Stafford Street, Norwich, NR2 3BD, United Kingdom'))
